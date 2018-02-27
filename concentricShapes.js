d2b0009e781061d46f99ea6491d79469dd6c0999
sophiavanvalkenburg <sophiavanvalkenburg@users.noreply.github.com>
2018-02-26 23:44:50 -0500

    addLayer debug/test

--- concentricShapes.js
+++ concentricShapes.js
@@ -1,6 +1,8 @@
 var BACKGROUND_COLOR = 200;
+var LAYER_PADDING = 10;
 
 var points = [];
+var outline = [];
 
 function drawLines(lines) {
     beginShape();
@@ -16,6 +18,30 @@ function drawLines(lines) {
     endShape(CLOSE);
 }
 
+function rotate2D(p, origin, angle) {
+    var x = p.x - origin.x;
+    var y = p.y - origin.y;
+    var resX = x * cos(angle) - y * sin(angle);
+    var resY = y * cos(angle) + x * sin(angle);
+    return Point(resX + origin.x, resY + origin.y);
+}
+
+function addLayer() {
+    var layerLines = [];
+    var line1 = outline[outline.length - 1];
+    var line2;
+    for (var i = 0; i < outline.length; i++) {
+        line2 = outline[i];
+        var v1 = lineToUnitVec(line1.b, line1.a);
+        var v2 = lineToUnitVec(line2.a, line2.b);
+        var angle = 2*PI - acos(v2.dot(v1));
+        //text(int(degrees(angle)), line2.a.x, line2.a.y);
+        var newP = rotate2D(Point(v1.x, v1.y), line2.a, angle / 2);
+        point(newP.x, newP.y);
+        line1 = line2;
+    }
+}
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(BACKGROUND_COLOR);
@@ -24,14 +50,15 @@ function setup() {
 }
 
 function mouseReleased(){
-    clear();
-    background(BACKGROUND_COLOR);
-    var outline = createOutline(points);
-    drawLines(outline);
-
-    points = [];
-    strokeWeight(3);
-    stroke(0)
+    if (points.length) {
+        clear();
+        background(BACKGROUND_COLOR);
+        outline = createOutline(points);
+        drawLines(outline);
+        points = [];
+    } else if (outline.length) {
+        addLayer();
+    } 
 }
 
 var lastX = 0;
@@ -41,6 +68,7 @@ function mouseDragged() {
         points.push(Point(mouseX, mouseY));
         lastX = mouseX;
         lastY = mouseY;
+        strokeWeight(3);
         point(mouseX, mouseY);
     }
 }
--- util.js
+++ util.js
@@ -21,3 +21,8 @@ function len(line) {
     var yDiff = line.a.y - line.b.y;
     return sqrt(xDiff * xDiff + yDiff * yDiff);
 }
+
+function lineToUnitVec(p1, p2) {
+    var v = createVector(p1.x - p2.x, p1.y - p2.y).normalize();
+    return v;
+}
